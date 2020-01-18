a852064d4fda0d43e421aa817b382f7055a220ac
adriana <adrianapenate98@gmail.com>
2020-01-18 12:01:21 +0000

    Refactor - Class name changed

--- src/test/java/YourOrderPleaseShould.java
+++ src/test/java/YourOrderPleaseShould.java
@@ -1,13 +1,12 @@
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class YourOrderPlease_ {
+public class YourOrderPleaseShould {
     @Test
     public void should_not_order_when_string_is_empty() {
         assertThat(YourOrderPlease.orderPhrase("")).isEqualTo("");
