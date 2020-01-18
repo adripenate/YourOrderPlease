a702b545d6a1b757598e9c0b77ad3070b19541e0
adriana <adrianapenate98@gmail.com>
2020-01-18 11:28:43 +0000

    Red test - should_order_when_string_has_three_words

--- src/test/java/YourOrderPlease_.java
+++ src/test/java/YourOrderPlease_.java
@@ -18,6 +18,11 @@ public class YourOrderPlease_ {
         assertThat(YourOrderPlease.orderPhrase("Jos2eph H1ello")).isEqualTo("H1ello Jos2eph");
     }
 
+    @Test
+    public void should_order_when_string_has_three_words() {
+        assertThat(YourOrderPlease.orderPhrase("Jos3eph H1ello lit2tle")).isEqualTo("H1ello lit2tle Jos3eph");
+    }
+
     private static class YourOrderPlease {
         public static String orderPhrase(String phrase) {
             if (!containsAnyWhiteSpace(phrase)) return phrase;
