bd9d216c1a5381e4bdb7a28babce305f5eba15d0
adriana <adrianapenate98@gmail.com>
2020-01-18 12:02:37 +0000

    Refactor - Tests' names changed

--- src/test/java/YourOrderPleaseShould.java
+++ src/test/java/YourOrderPleaseShould.java
@@ -8,22 +8,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class YourOrderPleaseShould {
     @Test
-    public void should_not_order_when_string_is_empty() {
+    public void not_order_when_string_is_empty() {
         assertThat(YourOrderPlease.orderPhrase("")).isEqualTo("");
     }
 
     @Test
-    public void should_not_order_when_string_has_one_word() {
+    public void not_order_when_string_has_one_word() {
         assertThat(YourOrderPlease.orderPhrase("H1ello")).isEqualTo("H1ello");
     }
 
     @Test
-    public void should_order_when_string_has_two_word() {
+    public void order_when_string_has_two_word() {
         assertThat(YourOrderPlease.orderPhrase("Jos2eph H1ello")).isEqualTo("H1ello Jos2eph");
     }
 
     @Test
-    public void should_order_when_string_has_three_words() {
+    public void order_when_string_has_three_words() {
         assertThat(YourOrderPlease.orderPhrase("Jos3eph H1ello lit2tle")).isEqualTo("H1ello lit2tle Jos3eph");
     }
 
