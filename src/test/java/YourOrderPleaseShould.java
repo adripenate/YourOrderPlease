0fad1b164487bfa37e37a96971650900700ab30a
adriana <adrianapenate98@gmail.com>
2020-01-18 12:09:01 +0000

    Refactor - Integer.parse operation moved to one place

--- src/test/java/YourOrderPleaseShould.java
+++ src/test/java/YourOrderPleaseShould.java
@@ -46,14 +46,14 @@ public class YourOrderPleaseShould {
 
         private static Comparator<String> byIndex(){
             return (firstWord, secondWord) -> {
-                firstWord = getNumberIn(firstWord);
-                secondWord = getNumberIn(secondWord);
-                return (Integer.parseInt(firstWord) > Integer.parseInt(secondWord))? 1 : -1;
+                int firstNumber = getNumberIn(firstWord);
+                int secondNumber = getNumberIn(secondWord);
+                return (firstNumber > secondNumber)? 1 : -1;
             };
         }
 
-        private static String getNumberIn(String word) {
-            return word.replaceAll("\\D+", "");
+        private static int getNumberIn(String word) {
+            return Integer.parseInt(word.replaceAll("\\D+", ""));
         }
     }
 }
